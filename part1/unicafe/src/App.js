76058309b979ab991fc022cbf0521c45e9e33fbc
Adam Morelli <amorelli@gmail.com>
2022-01-05 20:27:27 -0500

    Part 1.7 unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,14 +1,35 @@
 import React, { useState } from 'react'
 
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleSetGood = () => setGood(good + 1);
+  const handleSetNeutral = () => setNeutral(neutral + 1);
+  const handleSetBad = () => setBad(bad + 1);
+
+  const totalCount = good + neutral + bad;
+  const getAverage = () => (good-bad)/totalCount;
+
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <Button handleClick={handleSetGood} text="Good" />
+      <Button handleClick={handleSetNeutral} text="Neutral" />
+      <Button handleClick={handleSetBad} text="Bad" />
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Average: {getAverage()}</p>
+      <p>Positive: {good/totalCount}</p>
     </div>
   )
 }
